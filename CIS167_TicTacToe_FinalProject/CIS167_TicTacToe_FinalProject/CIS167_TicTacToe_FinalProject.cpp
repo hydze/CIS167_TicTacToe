ec41c0a0396696ae8fea3c388b75a7586d183cf8
hydze <hhyde@protonmail.com>
2023-04-19 01:04:33 -0400

    DISPLAY & ARRAY
    -created the ascii display for the board, using printf function to save on memory, since cout is a little heavier, as well as utilizing the replace with %c
    
    -have single char array created
    --use table_at for using a double array syntax

--- CIS167_TicTacToe_FinalProject/CIS167_TicTacToe_FinalProject/CIS167_TicTacToe_FinalProject.cpp
+++ CIS167_TicTacToe_FinalProject/CIS167_TicTacToe_FinalProject/CIS167_TicTacToe_FinalProject.cpp
@@ -1,8 +1,40 @@
-#include <iostream>
+﻿#include <iostream>
 #include <string>
+//making sure we dont move outside of memory
+#include <cassert>
 
 using namespace std;
+
+static char table[9] = {'-', '-', '-', '-', '-', '-', '-', '-', '-'};
+
+//getting the table into a format of a double array
+inline char& table_at(uint8_t x, uint8_t y) {
+    assert(x < 3 && y < 3);
+    return table[x + y * 3];
+};
+
+void displayBoard()
+{
+    printf(
+        "   a     b     c\n"
+        "      |     |\n"
+        "1  %c  |  %c  |  %c\n"
+        " _____|_____|_____\n"
+        "      |     |     \n"
+        "2  %c  |  %c  |  %c\n"
+        " _____|_____|_____\n"
+        "      |     |     \n"
+        "3  %c  |  %c  |  %c\n"
+        "      |     |\n",
+        table[0], table[1], table[2], table[3], table[4], table[5], table[6], table[7], table[8]
+    );
+}
+
 int main()
 {
+    displayBoard();
+
+    int tttarr[2][3];
+
     
 }
